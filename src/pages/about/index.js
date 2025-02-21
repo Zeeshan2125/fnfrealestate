0e980c7e6cef312fac403337544475a27794ce6c
Zeeshan2125 <zali86369@gmail.com>
2025-02-21 04:17:53 +0400

    new changes

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -19,7 +19,16 @@ import TeamData from '@/data/team';
 function AboutUs() {
   const agents = getProducts(TeamData, "buying", "featured", 3);
   const featureDataSorted = getProducts(featureData, "buying", "featured", 3);
-
+  const vision = [
+    {
+      title: "Vision",
+      shortDescription: "This is our vison This is our vison This is our vison This is our vison This is our vison This is our vison This is our vison"
+    },
+    {
+      title: "Mission",
+      shortDescription: "This is our Mission This is our Mission This is our Mission This is our Mission This is our Mission This is our Mission"
+    }
+  ]
   const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
     <button
       {...props}
@@ -57,6 +66,23 @@ function AboutUs() {
     slidesToScroll: 1,
     prevArrow: <SlickArrowLeft />,
     nextArrow: <SlickArrowRight />,
+
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   const blogSettings = {
@@ -84,46 +110,19 @@ function AboutUs() {
           classes="section-bg-1"
           servicebtn={true}
           iconTag={false}
-          data={featureDataSorted}
+          data={vision}
+          white={true}
           titleSectionData={{
             sectionClasses: "text-center",
-            subTitle: "Our Services",
-            title: "Our Main Focus",
+            subTitle: "Core Values",
+            title: "Our Vision & Mission",
           }}
         />
 
 
 
 
-        <div className="ltn__team-area pt-115 pb-90">
-          <Container>
-            <Row>
-              <Col lg={12}>
-                <TitleSection
-                  sectionClasses="text-center"
-                  headingClasses="section-subtitle-2"
-                  titleSectionData={{
-                    subTitle: "Team",
-                    title: "Property Agents",
-                  }}
-                />
-              </Col>
-            </Row>
-
-            <Row>
-              {agents.map((data, key) => {
-                const slug = productSlug(data.name);
-                return (
-                  <Col key={key} xs={12} sm={6} lg={4} >
-                    <TeamItem baseUrl="blog" data={data} slug={slug} additionalClassname="" />
-                  </Col>
-                );
-              })}
-            </Row>
-
-
-          </Container>
-        </div>
+    
 
 
 
@@ -151,14 +150,60 @@ function AboutUs() {
               className="ltn__testimonial-slider-5-active slick-arrow-1"
             >
               {testimonialData.map((data, key) => {
-                return (
-                  <TestimonialCarouselItem key={key} data={data} />
-                );
+                return <TestimonialCarouselItem key={key} data={data} />;
               })}
             </Slider>
           </Container>
         </div>
         {/* <!-- TESTIMONIAL AREA END --> */}
+
+
+        <div style={{ backgroundColor: "#01356d", }}>
+          <Container >
+            <div className="ltn__team-area pt-115 pb-90">
+              <Row>
+                <Col lg={12}>
+                  <TitleSection
+                    sectionClasses="text-center"
+                    headingClasses="section-subtitle-2"
+                    titleSectionData={{
+                      subTitle: "Our Team",
+                      title: "Meet Our Wonderful Team",
+                    }}
+                    white={true}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Slider
+                  {...testiMonialsettings}
+                  className="ltn__testimonial-slider-5-active slick-arrow-1"
+                >
+
+                  {agents.map((data, key) => {
+                    const slug = productSlug(data.name);
+                    return (
+                      // <Col key={key} xs={12} sm={6} lg={4} >
+                      <TeamItem key={key} baseUrl="blog" data={data} slug={slug} additionalClassname="" white={true} />
+                      // </Col>
+                    );
+                  })}
+                  {/* {testimonialData.map((data, key) => {
+                  return <TestimonialCarouselItem key={key} data={data} />;
+                })} */}
+                </Slider>
+                {/* {agents.map((data, key) => {
+                const slug = productSlug(data.name);
+                return (
+                  <Col key={key} xs={12} sm={6} lg={4} >
+                    <TeamItem baseUrl="blog" data={data} slug={slug} additionalClassname="" />
+                  </Col>
+                );
+              })} */}
+              </Row>
+            </div>
+          </Container>
+        </div>
 
         {/* <!-- BLOG AREA START (blog-3) -->  */}
         <div className="ltn__blog-area pb-70">
